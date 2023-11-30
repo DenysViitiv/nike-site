c29d6de8827a0ef9c15b761197ea4b729dd6373a
DenysViitiv <denys.viytiv@gmail.com>
2023-11-30 15:10:13 +0200

    nike site

---
+++ nike-react/src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import Services from "./Services";
+
+export {
+  Hero,
+  CustomerReviews,
+  PopularProducts,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  Services,
+  Footer,
+};
